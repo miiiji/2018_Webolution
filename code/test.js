02dfa94b1da4b097858af8378fe4434c93799882
NamJinWoo <cjsencks@naver.com>
2018-09-28 18:44:51 +0900

    butoon to main.html

--- code/test.js
+++ code/test.js
@@ -1,5 +1,5 @@
+/*landing JS*/
 window.onload = function() {
-    alert("!!!");
     var container = document.getElementById("container");
 
     var cW = container.offsetWidth;
@@ -123,3 +123,26 @@ window.onload = function() {
     tb.move();
 
 }
+
+
+/*Button JS*/
+
+
+
+// var animateButton = function(e) {
+//
+//   e.preventDefault;
+//   //reset animation
+//   e.target.classList.remove('animate');
+//
+//   e.target.classList.add('animate');
+//   setTimeout(function(){
+//     e.target.classList.remove('animate');
+//   },700);
+// };
+//
+// var bubblyButtons = document.getElementsByClassName("bubbly-button");
+//
+// for (var i = 0; i < bubblyButtons.length; i++) {
+//   bubblyButtons[i].addEventListener('click', animateButton, false);
+// }
