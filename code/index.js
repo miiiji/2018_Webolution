8b3db677046e47b42ee7d1b648ba5481c927a1bf
NamJinWoo <cjsencks@naver.com>
2018-09-29 01:47:49 +0900

    modified

--- code/index.js
+++ code/index.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-  //sign up 하는 함수
-=======
-
-
   //sign up 하는 함수 
->>>>>>> ff80c7a6c9dcfe6a4d760bc4a870631e75ee1913
   function signUp() {
     var id = $("#su_id").val();
     var pw = $("#su_pw").val();
     var cf = $("#su_cf").val();
 
-<<<<<<< HEAD
     if (pw != cf) {
       alert("Password does not match the confirm password.");
       return;
@@ -39,11 +32,10 @@
 
   //로그인 (sing in) 하는 함수
   function signIn() {
-=======
     if(pw != cf) {
         alert("Password does not match the confirm password.");
         return;
-    }       
+    }
 
     firebase.auth().createUserWithEmailAndPassword(id, pw)
             .then(function() {
@@ -53,9 +45,9 @@
                         E_mail : id
                 }
                 userRef.set(data);
-                console.log(id.split('@')[0]); 
+                console.log(id.split('@')[0]);
                 location.href="./Main.html";
-                
+
             })
             .catch(function(e) {
                  alert("bb");
@@ -67,7 +59,6 @@
 
 //로그인 (sing in) 하는 함수
  function signIn() {
->>>>>>> ff80c7a6c9dcfe6a4d760bc4a870631e75ee1913
     var id = $("#si_id").val();
     var pw = $("#si_pw").val();
     firebase.auth().signInWithEmailAndPassword(id, pw)
@@ -85,7 +76,6 @@
       });
   }
 
-<<<<<<< HEAD
 
   //현재 로그인 상태를 감지해서 로그인이 되었으면 화면에 띄우고 아니면 경고창 띄우는 함수
   firebase.auth().onAuthStateChanged(function(user) {
@@ -94,5 +84,3 @@
       document.getElementById("log-button").style.color = "rgb(233,65,103)";
     }
   });
-=======
->>>>>>> ff80c7a6c9dcfe6a4d760bc4a870631e75ee1913
